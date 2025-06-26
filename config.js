4d07f4177e1eebe3d3a0d85210f265132454753e
MappingMetabolismofIstanbul <dogatmn@gmail.com>
2025-06-26 22:40:09 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,298 @@
+var config = {
+    // style: 'mapbox://styles/mapbox/streets-v12',
+    // leave commented to use Mapbox Standard Style
+    style: 'mapbox://styles/dogatmn/cm4v9fem6000p01sf12f769y5',
+    accessToken: 'pk.eyJ1IjoiZG9nYXRtbiIsImEiOiJjbHlyMTJmcXgwMjQ3MmtzbTRsdm1pejdsIn0.vcEjL7lt8OoOPMP5o1PM6g',
+    showMarkers: false,
+    markerColor: '#3FB1CE',
+    //projection: 'equirectangular',
+    //Read more about available projections here
+    //https://docs.mapbox.com/mapbox-gl-js/example/projections/
+    inset: true,
+    insetOptions: {
+        markerColor: 'orange'
+    },
+    insetPosition: 'bottom-right',
+    theme: 'light',
+    use3dTerrain: false, //set true for enabling 3D maps.
+    auto: false,
+    title: 'Kömür Mavnası / Coal Barge',
+    subtitle: '',
+    byline: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
+    footer: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
+    chapters: [
+        {
+             id: 'First',
+            alignment: 'centered',
+            hidden: true,
+            title: '',
+            image: '',
+            description: '',
+            location: {
+                center: [28.947, 41.064],
+                zoom: 18,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+         {
+            id: 'third',
+            alignment: 'left',
+            hidden: true,
+            title: '',
+            image: '',
+            description: '',
+            location: {
+                center: [28.946, 41.062],
+                zoom: 17,
+                pitch: 0,
+                bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'second',
+            alignment: 'right',
+            hidden: true,
+            title: '',
+            image: '',
+            description: ' ',
+            location: {
+                center: [28.942, 41.054],
+                zoom: 17,
+                pitch: 0,
+                bearing: 0
+                // flyTo additional controls-
+                // These options control the flight curve, making it move
+                // slowly and zoom out almost completely before starting
+                // to pan.
+                //speed: 2, // make the flying slow
+                //curve: 1, // change the speed at which it zooms out
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'thirteen',
+            alignment: 'left',
+            hidden: true,
+            title: '',
+            image: '',
+            description: '',
+            location: {
+                center: [28.944, 41.043],
+                zoom: 17,
+                pitch: 0,
+                bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+         {
+            id: 'four',
+            alignment: 'left',
+            hidden: true,
+            title: '',
+            image: '',
+            description: '',
+            location: {
+                center: [28.953, 41.033],
+                zoom: 17,
+                pitch: 0,
+                bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+            
+        },
+          {
+            id: 'five',
+            alignment: 'left',
+            hidden: true,
+            title: '',
+            image: '',
+            description: '',
+            location: {
+                center: [28.965, 41.027],
+                zoom: 17,
+                pitch: 0,
+                bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+            
+        },
+         {
+            id: 'six',
+            alignment: 'left',
+            hidden: true,
+            title: '',
+            image: '',
+            description: '',
+            location: {
+                center: [28.981, 41.022],
+                zoom: 17,
+                pitch: 0,
+                bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+            
+        },
+         {
+            id: 'seven',
+            alignment: 'left',
+            hidden: true,
+            title: '',
+            image: '',
+            description: '',
+            location: {
+                center: [28.996, 41.028],
+                zoom: 17,
+                pitch: 0,
+                bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+            
+        },
+          {
+            id: 'eight',
+            alignment: 'left',
+            hidden: true,
+            title: '',
+            image: '',
+            description: '',
+            location: {
+                center: [29.014, 41.038],
+                zoom: 17,
+                pitch: 0,
+                bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+            
+        },
+         {
+            id: 'nine',
+            alignment: 'left',
+            hidden: true,
+            title: '',
+            image: '',
+            description: '',
+            location: {
+                center: [29.033, 41.049],
+                zoom: 17,
+                pitch: 0,
+                bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+            
+        },
+          {
+            id: 'ten',
+            alignment: 'left',
+            hidden: true,
+            title: '',
+            image: '',
+            description: '',
+            location: {
+                center: [29.039, 41.059],
+                zoom: 17,
+                pitch: 0,
+                bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+            
+        },
+          {
+            id: 'eleven',
+            alignment: 'left',
+            hidden: false,
+            title: '',
+            image: 'https://i.ibb.co/S4wkcY5b/MEF-AAP-027-BAP-2025-06-23-18-15-49.png',
+            description: '',
+            location: {
+                center: [29.038, 41.061],
+                zoom: 17,
+                pitch: 0,
+                bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+            
+        },
+          {
+            id: 'twelve',
+            alignment: 'right',
+            hidden: false,
+            title: '',
+            image: 'https://i.ibb.co/8LLrgw1w/MEF-AAP-027-BAP-2025-06-20-13-24-59.png',
+            description: '',
+            location: {
+                center: [29.038, 41.061],
+                zoom: 18,
+                pitch: 0,
+                bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+            
+        },   
+    ]
+};
